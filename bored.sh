ea5970b4413eaca1a4ce86feaeb87146d061c9c0
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-13 08:21:14 +0000

    Bored api Bash Script

---
+++ bored.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Richard Deodutt
+#08/15/2022
+#This script is meant to gather data from a API that suggests random activities for bored people.
+#Issues
+
+#Url for the api to get a random activity suggestion
+Url='http://www.boredapi.com/api/activity'
+
+#Curl the api sending output to stdout
+curl $Url
